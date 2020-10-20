3e15642d8b0121c648dadea62648f794b7d916bb
izarc6 <45626576+izarc6@users.noreply.github.com>
2020-10-20 15:02:05 +0200

    Commit inicial, añadidos files del profe

---
+++ src/java/perbd/DBProperties.java
@@ -0,0 +1,9 @@
+package perbd;
+
+public class DBProperties {
+    static public final String host = "localhost";
+    static public final String port = "8889";   // CAMBIADO DE 3306 PORQUE SI NO NO FUNCIONABA CON MAMP
+    static public final String db = "peliculas";
+    static public final String user = "peliculas";
+    static public final String pass = "peliculas";
+}
