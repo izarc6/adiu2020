dbefabdda7eb65786a6ab363e49af69e60f5493f
izarc6 <izar.castorina@gmail.com>
2020-10-21 16:21:30 +0200

    Sacada de rabo n.1

--- src/java/perbd/DBProperties.java
+++ src/java/perbd/DBProperties.java
@@ -2,7 +2,7 @@ package perbd;
 
 public class DBProperties {
     static public final String host = "localhost";
-    static public final String port = "8889";   // CAMBIADO DE 3306 PORQUE SI NO NO FUNCIONABA CON MAMP
+    static public final String port = "3306";
     static public final String db = "peliculas";
     static public final String user = "peliculas";
     static public final String pass = "peliculas";
